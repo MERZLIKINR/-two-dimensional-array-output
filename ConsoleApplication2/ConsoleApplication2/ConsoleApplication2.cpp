e76f153adbb045af12b2807e1bd69164db666147
MERZLIKINR <54219045+MERZLIKINR@users.noreply.github.com>
2020-09-16 22:10:02 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<ctime>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	system("color 04");
+
+	const int ROW = 10;
+	const int COLLUM = 10;
+
+	int arr[ROW][COLLUM]{};
+
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < COLLUM; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}       
+	}
+
+	for (int i = 0; i < ROW; i++)
+	{ 
+		for (int j = 0; j < COLLUM; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+
+		cout << endl;
+	}
+
+	system("pause"); 
+}
